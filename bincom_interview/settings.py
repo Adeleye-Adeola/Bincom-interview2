0155015138fd5b9dfff431b428f8cbad9b4942b1
Adeleye-Adeola <mightydeveloper500@gmail.com>
2025-02-12 10:50:12 +0100

    Finshed work

--- bincom_interview/settings.py
+++ bincom_interview/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'election.apps.ElectionConfig'
+    'election.apps.ElectionConfig',
     'whitenoise.runserver_nostatic',
 ]
 
